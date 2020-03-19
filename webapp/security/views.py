82ca745eb726e044deefd175ad7b0ee0ad934cfa
Carlos Wu <carlos.fei@canonical.com>
2020-03-19 16:27:07 +0000

    Handle cve routes

--- webapp/security/views.py
+++ webapp/security/views.py
@@ -5,7 +5,6 @@ from math import ceil
 
 # Packages
 import flask
-import re
 from feedgen.entry import FeedEntry
 from feedgen.feed import FeedGenerator
 from marshmallow import EXCLUDE
@@ -277,10 +276,4 @@ def cve_index():
 
 
 def cve(cve_id):
-    match_cves = re.compile(r"cve-\d{4}-\d{4,7}")
-    if cve_id == "cve":
-        return flask.render_template("security/cve/index.html")
-    elif match_cves.search(cve_id) is not None:
-        return flask.render_template("security/cve/cve.html")
-    else:
-        flask.abort(404)
+    return flask.render_template("security/cve/cve.html")
